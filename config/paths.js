e58f99f2b8c7eaa83d9e53fbc765f448151a4779
Philipp Beck <dertieran@gmail.com>
2016-11-29 13:11:10 +0000

    :file_folder: New project structure

--- config/paths.js
+++ config/paths.js
@@ -29,10 +29,10 @@ module.exports = {
   appBuild: resolveApp('build'),
   appPublic: resolveApp('public'),
   appHtml: resolveApp('public/index.html'),
-  appIndexJs: resolveApp('src/index.js'),
+  appIndexJs: resolveApp('source/js/index.js'),
   appPackageJson: resolveApp('package.json'),
-  appSrc: resolveApp('src'),
-  testsSetup: resolveApp('src/setupTests.js'),
+  appSrc: resolveApp('source/js'),
+  testsSetup: resolveApp('source/setupTests.js'),
   appNodeModules: resolveApp('node_modules'),
   ownNodeModules: resolveApp('node_modules'),
   nodePaths: nodePaths
@@ -44,12 +44,12 @@ module.exports = {
 if (__dirname.indexOf(path.join('packages', 'react-scripts', 'config')) !== -1) {
   module.exports = {
     appBuild: resolveOwn('../../../build'),
-    appPublic: resolveOwn('../template/public'),
-    appHtml: resolveOwn('../template/public/index.html'),
-    appIndexJs: resolveOwn('../template/src/index.js'),
+    appPublic: resolveOwn('../template/source'),
+    appHtml: resolveOwn('../template/source/index.html'),
+    appIndexJs: resolveOwn('../template/source/js/index.js'),
     appPackageJson: resolveOwn('../package.json'),
-    appSrc: resolveOwn('../template/src'),
-    testsSetup: resolveOwn('../template/src/setupTests.js'),
+    appSrc: resolveOwn('../template/source/js'),
+    testsSetup: resolveOwn('../template/source/setupTests.js'),
     appNodeModules: resolveOwn('../node_modules'),
     ownNodeModules: resolveOwn('../node_modules'),
     nodePaths: nodePaths
