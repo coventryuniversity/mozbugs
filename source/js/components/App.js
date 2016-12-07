c8fc5a1f0e7e5eee9a6b7f7093eaa95dbd9ea78e
Philipp Beck <dertieran@gmail.com>
2016-12-07 01:12:17 +0000

    Updated style
    - appbar
    - fixed sidebar

--- source/js/components/App.js
+++ source/js/components/App.js
@@ -3,7 +3,6 @@ import Filter from './Filter';
 import BugContainer from './Bugs/BugContainer';
 import Store from '../Store';
 import { FilterOptions } from '../Constants';
-import Spin from 'antd/lib/spin';
 import 'antd/lib/spin/style/css';
 
 class App extends Component {
@@ -39,20 +38,23 @@ class App extends Component {
   }
 
   render() {
+    const appbarIcon = this.state.isLoading ? 'fa-spinner fa-pulse' : 'fa-bug'
     return (
-      <div className="app">
-        <div className="app-title">
-          <i className={`app-title-icon fa fa-bug fa-fw`}/>
-          <span className="app-title-text">Moz Bugs!</span>
+      <div>
+        <div className="appbar">
+          <i className={`appbar-icon fa fa-fw ${appbarIcon}`}/>
+          <span className="appbar-title">Moz Bugs!</span>
         </div>
-        <div className="app-container">
+        <div className="sidebar">
           <Filter onChange={this.onFilterChange.bind(this)}/>
-          {
-            this.state.filterOptions.length === 0 ? <div className="response">No options selected</div> :
-            this.state.isLoading ? <div className="response"><Spin size="large" /></div> :
-            this.state.bugs.length === 0 ? <div className="response">No matching bugs</div> :
-            <BugContainer bugs={this.state.bugs}/>
-          }
+        </div>
+        <div className="content">
+            {
+              this.state.filterOptions.length === 0 ? <div className="response">No options selected</div> :
+              this.state.isLoading ? <div className="response">Loading...</div> :
+              this.state.bugs.length === 0 && !this.state.isLoading ? <div className="response">No matching bugs</div> :
+              <BugContainer bugs={this.state.bugs}/>
+            }
         </div>
       </div>
     );
