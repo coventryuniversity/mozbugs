15828b6cafc444f12a807db358245b4ffdc27a55
Philipp Beck <dertieran@gmail.com>
2016-12-08 17:24:00 +0000

    Started on making bugs pinnalble
    Right now it is just for display

--- source/js/components/Bugs/BugItem.js
+++ source/js/components/Bugs/BugItem.js
@@ -3,20 +3,29 @@ import capitalize from 'lodash/capitalize';
 import { buildBugzillaLink } from '../../Utils';
 import Card from 'antd/lib/card';
 import Tag from 'antd/lib/tag';
-// import Button from 'antd/lib/button';
+import Button from 'antd/lib/button';
 import 'antd/lib/card/style/css';
 import 'antd/lib/tag/style/css';
-// import 'antd/lib/button/style/css';
+import 'antd/lib/button/style/css';
 
 class Bug extends Component {
+  constructor() {
+    super()
+    this.state = {
+      pinned: false
+    }
+  }
 
   onClick(event) {
     event.preventDefault()
+    this.setState({
+      pinned: !this.state.pinned
+    })
   }
 
   render() {
     const bug = this.props.bug
-    // const pin = <Button type="ghost" icon="pushpin-o" onClick={this.onClick.bind(this)} />
+    const pin = <Button type={this.state.pinned ? 'primary' : 'ghost'} icon="pushpin" onClick={this.onClick.bind(this)} />
     const statusColor = bug.status === 'NEW' ? '#8BC34A' :
       bug.status === 'ASSIGNED' ? '#C13832' :
       bug.status === 'REOPENED' ? '#FF9800' :
@@ -24,7 +33,7 @@ class Bug extends Component {
 
     return (
       <a className='bug' href={buildBugzillaLink(bug)} target="_blank" >
-        <Card title={capitalize(bug.summary)} /*extra={pin}*/>
+        <Card title={capitalize(bug.summary)} extra={pin}>
           <Tag color={statusColor}>
             {capitalize(bug.status)}
           </Tag>
